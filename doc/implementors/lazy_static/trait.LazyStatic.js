0c76f3163476cbaf52660fe370e33e817efef138
lmorchard <lmorchard@users.noreply.github.com>
2023-07-25 21:55:12 +0000

    deploy: 0ea11f51f4dd10326cd0cddc605fb8e8236a44ef

---
+++ doc/implementors/lazy_static/trait.LazyStatic.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"fossilizer":[["impl <a class=\"trait\" href=\"https://docs.rs/lazy_static/1.4.0/lazy_static/trait.LazyStatic.html\" title=\"trait lazy_static::LazyStatic\">LazyStatic</a> for <a class=\"struct\" href=\"fossilizer/config/struct.DEFAULT_CONFIG.html\" title=\"struct fossilizer::config::DEFAULT_CONFIG\">DEFAULT_CONFIG</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/source-files.js
+++ doc/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"fossilizer":["",[["db",[],["activities.rs","actors.rs"]]],["activitystreams.rs","app.rs","config.rs","db.rs","lib.rs","mastodon.rs","templates.rs"]]\
+"fossilizer":["",[["db",[],["activities.rs","actors.rs"]],["templates",[],["contexts.rs"]]],["activitystreams.rs","app.rs","config.rs","db.rs","lib.rs","mastodon.rs","site_generator.rs","templates.rs"]]\
 }');
 createSourceSidebar();
