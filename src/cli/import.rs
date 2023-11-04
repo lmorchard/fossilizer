ea9084daf11cea2e0a7eb31302bfe63cc5f69784
Les Orchard <lmorchard@github.com>
2023-11-03 22:03:59 -0700

    misc progress on importer refactor

--- src/cli/import.rs
+++ src/cli/import.rs
@@ -1,10 +1,13 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Args;
+use flate2::read::GzDecoder;
 use std::convert::From;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
+use std::io::prelude::*;
 use std::path::{Component, PathBuf};
+use tar::Archive;
 
 use fossilizer::{activitystreams, archives, config, db, mastodon};
 
@@ -27,32 +30,27 @@ rework plan:
 
 pub async fn command(args: &ImportArgs) -> Result<(), Box<dyn Error>> {
     let config = config::config()?;
+    
     let data_path = PathBuf::from(&config.data_path);
     fs::create_dir_all(&data_path)?;
 
+    let media_path = config.media_path();
+    fs::create_dir_all(&media_path)?;
+    
+    let importer = MastodonImporter::new(media_path);
+
     for filename in &args.filenames {
         let filename: PathBuf = filename.into();
-        // let file = File::open(filepath.as_path())?;
 
         info!("Importing {:?}", filename);
 
-        let visitor = MastodonImporter::new();
-
-        // todo: how to do this with the trait?!
-        match filename.extension().unwrap().to_str().unwrap() {
-            "gz" => archives::scan_tar(filename, true, &visitor)?,
-            "tar" => archives::scan_tar(filename, false, &visitor)?,
-            "zip" => archives::scan_zip(filename, &visitor)?,
-            _ => println!("NO SCANNER AVAILABLE"),
-        }
+        importer.import(filename)?;
 
         /*
         let conn = db::conn()?;
 
         let mut export = mastodon::Export::from(filename);
 
-        let media_path = config.media_path();
-        fs::create_dir_all(&media_path)?;
         debug!("extracting media to {:?}", media_path);
         export.unpack_media(&media_path)?;
 
@@ -73,38 +71,77 @@ pub async fn command(args: &ImportArgs) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-use std::io::prelude::*;
-
 pub struct MastodonImporter {
-
+    media_path: PathBuf,
 }
 
 impl MastodonImporter {
-    pub fn new() -> Self {
-        Self { }
+    pub fn new(media_path: PathBuf) -> Self {
+        Self {
+            media_path
+        }
     }
 
-    fn handle_outbox(&self, read: &mut impl Read) -> Result<()> {
-        let bytes = read.bytes();
-        println!("OUTBOX: {:?}", bytes.count());
+    pub fn import(&self, filepath: PathBuf) -> Result<()> {
+        let filepath = filepath.as_path();
+        let file = File::open(filepath)?;
+
+        // todo: do something with filemagic here to auto-detect archive format based on file contents?
+        let extension = filepath
+            .extension()
+            .ok_or(anyhow!("no file extension"))?
+            .to_str()
+            .ok_or(anyhow!("no file extension"))?;
+        match extension {
+            "gz" => self.import_tar(file, true)?,
+            "tar" => self.import_tar(file, false)?,
+            "zip" => self.import_zip(file)?,
+            _ => println!("NO SCANNER AVAILABLE"),
+        };
+
         Ok(())
     }
 
-    fn handle_media_attachment<R>(&self, dest_path: &PathBuf, read: &mut R) -> Result<()>
-    where
-        R: ?Sized,
-        R: Read,
-    {
-        let bytes = read.bytes();
-        println!("MEDIA: {:?} {:?}", dest_path, bytes.count());
+    pub fn import_tar(&self, file: File, use_gzip: bool) -> Result<()> {
+        // hack: this optional decompression seems funky, but it works
+        let file: Box<dyn Read> = if use_gzip {
+            Box::new(GzDecoder::new(file))
+        } else {
+            Box::new(file)
+        };
+        let mut archive = Archive::new(file);
+        let entries = archive.entries()?;
+        for entry in entries {
+            let mut entry = entry?;
+            let entry_path: PathBuf = entry.path()?.into();
+            self.handle_entry(&entry_path, &mut entry)?;
+        }
         Ok(())
     }
-}
 
-impl archives::ArchiveVisitor for MastodonImporter {
-    fn visit(&self, path: &PathBuf, read: &mut impl Read) -> Result<()> {
+    pub fn import_zip(&self, file: File) -> Result<()> {
+        let mut archive = zip::ZipArchive::new(file).unwrap();
+
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+            let outpath = match file.enclosed_name() {
+                Some(path) => path.to_owned(),
+                None => continue,
+            };
+            // is this really the best way to detect that an entry isn't a directory?
+            if !(*file.name()).ends_with('/') {
+                self.handle_entry(&outpath, &mut file)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_entry(&self, path: &PathBuf, read: &mut impl Read) -> Result<()> {
         if path.ends_with("outbox.json") {
             self.handle_outbox(read)?;
+        } else if path.ends_with("actor.json") {
+            self.handle_actor(read)?;
         } else if path.to_str().unwrap().contains("media_attachments") {
             // HACK: some exports seem to have leading directory paths before `media_attachments`, so strip that off
             let normalized_path: PathBuf = path
@@ -121,6 +158,28 @@ impl archives::ArchiveVisitor for MastodonImporter {
                 self.handle_media_attachment(path, read)?;
             }
         }
+        Ok(())
+    }
+
+    fn handle_outbox(&self, read: &mut impl Read) -> Result<()> {
+        let bytes = read.bytes();
+        println!("OUTBOX: {:?}", bytes.count());
+        Ok(())
+    }
+
+    fn handle_actor(&self, read: &mut impl Read) -> Result<()> {
+        let bytes = read.bytes();
+        println!("ACTOR: {:?}", bytes.count());
+        Ok(())
+    }
+
+    fn handle_media_attachment<R>(&self, dest_path: &PathBuf, read: &mut R) -> Result<()>
+    where
+        R: ?Sized,
+        R: Read,
+    {
+        let bytes = read.bytes();
+        println!("MEDIA: {:?} {:?}", dest_path, bytes.count());
         Ok(())
     }
 }
