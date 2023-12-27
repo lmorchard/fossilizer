0ea9fb4b5010977babfb98a6e474d4a6a341f815
Les Orchard <lmorchard@github.com>
2023-12-27 00:25:45 -0800

    generate index.json; use index.json to build a date nav selector

--- src/cli/build.rs
+++ src/cli/build.rs
@@ -11,6 +11,9 @@ pub struct BuildArgs {
     /// Skip building index page
     #[arg(long)]
     skip_index: bool,
+    /// Skip building index JSON page
+    #[arg(long)]
+    skip_index_json: bool,
     /// Skip building pages for activities
     #[arg(long)]
     skip_activities: bool,
@@ -25,6 +28,7 @@ pub struct BuildArgs {
 pub async fn command(args: &BuildArgs) -> Result<(), Box<dyn Error>> {
     let clean = args.clean;
     let skip_index = args.skip_index;
+    let skip_index_json = args.skip_index_json;
     let skip_activities = args.skip_activities;
     let skip_assets = args.skip_assets;
 
@@ -43,7 +47,7 @@ pub async fn command(args: &BuildArgs) -> Result<(), Box<dyn Error>> {
         site_generator::copy_web_assets(&config.build_path).unwrap();
     }
 
-    if !skip_activities || !skip_index {
+    if !skip_activities || !skip_index || !skip_index_json {
         let tera = templates::init().unwrap();
         let db_conn = db::conn().unwrap();
         let db_activities = db::activities::Activities::new(&db_conn);
@@ -54,6 +58,10 @@ pub async fn command(args: &BuildArgs) -> Result<(), Box<dyn Error>> {
             site_generator::plan_activities_pages(&config.build_path, &db_activities).unwrap();
         if !skip_index {
             site_generator::generate_index_page(&config.build_path, &day_entries, &tera).unwrap();
+        }
+        if !skip_index_json {
+            site_generator::generate_index_json(&config.build_path, &day_entries)
+                .unwrap();
         }
         if !skip_activities {
             site_generator::generate_activities_pages(
