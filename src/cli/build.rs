3c221f390e1f131b0d7291eca618941dd81faca5
Les Orchard <lmorchard@github.com>
2024-01-05 23:45:52 -0800

    simplify build command by removing threading

--- src/cli/build.rs
+++ src/cli/build.rs
@@ -1,10 +1,7 @@
 use anyhow::Result;
 use clap::Args;
 use fossilizer::{config, db, site_generator, templates};
-
 use std::error::Error;
-
-use std::thread;
 
 #[derive(Debug, Args)]
 pub struct BuildArgs {
@@ -42,46 +39,31 @@ pub async fn command(args: &BuildArgs) -> Result<(), Box<dyn Error>> {
 
     site_generator::setup_build_path(&config.build_path, &clean)?;
 
-    let threads = vec![
-        thread::spawn(move || -> Result<()> {
-            let config = config::config().unwrap();
-            if !skip_assets {
-                site_generator::copy_web_assets(&config.build_path).unwrap();
-            }
-            Ok(())
-        }),
-        thread::spawn(move || -> Result<()> {
-            let config = config::config().unwrap();
-            let tera = templates::init().unwrap();
-            let db_conn = db::conn().unwrap();
-            let db_activities = db::activities::Activities::new(&db_conn);
-            let db_actors = db::actors::Actors::new(&db_conn);
+    if !skip_assets {
+        site_generator::copy_web_assets(&config.build_path).unwrap();
+    }
 
-            if !skip_activities || !skip_index {
-                let actors = db_actors.get_actors_by_id().unwrap();
-                let day_entries =
-                    site_generator::plan_activities_pages(&config.build_path, &db_activities)
-                        .unwrap();
-                if !skip_index {
-                    site_generator::generate_index_page(&config.build_path, &day_entries, &tera)
-                        .unwrap();
-                }
-                if !skip_activities {
-                    site_generator::generate_activities_pages(
-                        &config.build_path,
-                        &tera,
-                        &actors,
-                        &day_entries,
-                    )
-                    .unwrap();
-                }
-            }
-            Ok(())
-        }),
-    ];
+    if !skip_activities || !skip_index {
+        let tera = templates::init().unwrap();
+        let db_conn = db::conn().unwrap();
+        let db_activities = db::activities::Activities::new(&db_conn);
+        let db_actors = db::actors::Actors::new(&db_conn);
 
-    for t in threads {
-        t.join().unwrap()?
+        let actors = db_actors.get_actors_by_id().unwrap();
+        let day_entries =
+            site_generator::plan_activities_pages(&config.build_path, &db_activities).unwrap();
+        if !skip_index {
+            site_generator::generate_index_page(&config.build_path, &day_entries, &tera).unwrap();
+        }
+        if !skip_activities {
+            site_generator::generate_activities_pages(
+                &config.build_path,
+                &tera,
+                &actors,
+                &day_entries,
+            )
+            .unwrap();
+        }
     }
 
     info!("Build finished");
