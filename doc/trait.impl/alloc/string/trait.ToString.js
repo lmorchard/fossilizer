bebc8fe2414ee8d02a59eca8122daa79615398a4
lmorchard <lmorchard@users.noreply.github.com>
2025-12-07 19:34:13 +0000

    deploy: ae0d3eb15f489b31e4375147c51b33218420c944

--- doc/trait.impl/alloc/string/trait.ToString.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["fossilizer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"fossilizer/db/activities/enum.ActivitySchema.html\" title=\"enum fossilizer::db::activities::ActivitySchema\">ActivitySchema</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[330]}
---
+++ doc/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fossilizer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"fossilizer/db/activities/enum.ActivitySchema.html\" title=\"enum fossilizer::db::activities::ActivitySchema\">ActivitySchema</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[319]}
