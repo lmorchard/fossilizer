6b9ccfa7a0744cdf93d59e218720f589a7cbc613
lmorchard <lmorchard@users.noreply.github.com>
2026-07-23 23:24:00 +0000

    deploy: 3198623b2700181c177f0bcaf4a9cb258cc9d9e5

--- doc/trait.impl/core/str/traits/trait.FromStr.js
+++ doc/trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["fossilizer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"fossilizer/db/activities/enum.ActivitySchema.html\" title=\"enum fossilizer::db::activities::ActivitySchema\">ActivitySchema</a>",0]]]]);
+    const implementors = Object.fromEntries([["fossilizer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"fossilizer/db/activities/enum.ActivitySchema.html\" title=\"enum fossilizer::db::activities::ActivitySchema\">ActivitySchema</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
